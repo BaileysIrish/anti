330dbc581f683e529634262b68521e8f96ac9f62
BaileysIrish <kyu8873@gmail.com>
2026-02-01 15:42:16 +0900

    fix: wrangler.toml에 pages_build_output_dir 추가

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,7 +2,26 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
     default: {
-        runtime: "edge", // 가장 핵심적인 설정
+        override: {
+            wrapper: "cloudflare-node",
+            converter: "edge",
+            proxyExternalRequest: "fetch",
+            incrementalCache: "dummy",
+            tagCache: "dummy",
+            queue: "dummy",
+        },
+    },
+    edgeExternals: ["node:crypto"],
+    middleware: {
+        external: true,
+        override: {
+            wrapper: "cloudflare-edge",
+            converter: "edge",
+            proxyExternalRequest: "fetch",
+            incrementalCache: "dummy",
+            tagCache: "dummy",
+            queue: "dummy",
+        },
     },
 };
 
