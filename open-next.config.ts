61c887769156b7dad5ff3b0736ee59dae6a6dab2
BaileysIrish <kyu8873@gmail.com>
2026-02-01 15:38:37 +0900

    fix: open-next.config.ts 추가

---
+++ open-next.config.ts
@@ -0,0 +1,9 @@
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
+
+const config: OpenNextConfig = {
+    default: {
+        runtime: "edge", // 가장 핵심적인 설정
+    },
+};
+
+export default config;
